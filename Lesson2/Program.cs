018079de8ec5a923b2caaf2894e7af4da1970858
d.ryabchuk <d.ryabchuk@pnn.in.ua>
2023-08-08 13:06:49 +0300

    Add project files.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Xml;
+namespace Lesson2
+{
+    public class App
+    {
+        public static void Main()
+        {
+            byte a = 0b00001111;
+            byte b = (byte)(a<<4);
+
+            Console.WriteLine(Convert.ToString(b,16));
+            Console.WriteLine(b);
+        }
+    }
+}
